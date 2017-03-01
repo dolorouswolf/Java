197a9dbd4dced8978af769213c0a442a60264705
zhangmin <zminds@163.com>
2017-03-01 12:49:34 +0800

    bugfix：递归二分查找返回值总是-1，因为没有吧最内层递归的返回值传递出来

--- src/Algorithm/Jd/BSearch.java
+++ src/Algorithm/Jd/BSearch.java
@@ -15,10 +15,12 @@ public class BSearch {
     public static void main(String[] args) {
         int[] iarray = {1, 2, 5, 9, 11, 15, 18, 19, 20, 21};
         System.out.println("测试不存在的key");
-        BSearch.bsearch(iarray, 0, iarray.length - 1, 10);
+        System.out.println("result:   " + BSearch.bsearch(iarray, 0, iarray.length - 1, 10));
         System.out.println("测试整个数组的值");
         for (int i = 0; i < iarray.length; i++) {
-            BSearch.bsearch(iarray, 0, iarray.length - 1, iarray[i]);
+            int result = 0;
+            result = BSearch.bsearch(iarray, 0, iarray.length - 1, iarray[i]);
+            System.out.println("result:  " + result);
         }
         System.out.println("循环二分查找：");
         int result1 = 0;
@@ -38,22 +40,22 @@ public class BSearch {
     public static int bsearch(int[] array, int start, int end, int key) {
 
         int mid = (end + start) / 2;
-
-
+        //默认找不到时返回-1
+        int result = -1;
         if (start <= end) {
             System.out.println("Current: start[" + start + "] " + array[start]
                     + " end[" + end + "] " + array[end] +
                     " mid[" + mid + "]" + array[mid]);
 
             if (array[mid] < key) {
-                bsearch(array, mid + 1, end, key);
+                result = bsearch(array, mid + 1, end, key);
             } else if (array[mid] > key) {
-                bsearch(array, start, mid - 1, key);
+                result = bsearch(array, start, mid - 1, key);
             } else {
-                return mid;
+                result = mid;
             }
         }
-        return -1;
+        return result;
     }
 
     //循环法二分查找
