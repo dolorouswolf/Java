c867b2ae36ffff468a2047d477f71869e56a00d9
zhangmin <zminds@163.com>
2017-03-02 21:09:33 +0800

    update: 使用循环数组实现

--- src/Algorithm/Jd/queue/ArrayQueue.java
+++ src/Algorithm/Jd/queue/ArrayQueue.java
@@ -13,46 +13,70 @@ public class ArrayQueue {
         ArrayQueue queue = new ArrayQueue();
         for (int i = 0; i < 10; i++) {
             queue.insert(i);
-            System.out.println(queue.peekFront()+" "+queue.peekEnd());
+            System.out.println(queue.peekFront() + " " + queue.peekEnd());
         }
 
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 5; i++) {
             queue.remove();
-            System.out.println(queue.peekFront()+" "+queue.peekEnd());
+            System.out.println(queue.peekFront() + " " + queue.peekEnd());
 
         }
 
+        for (int i = 10; i < 15; i++) {
+            queue.insert(i);
+            System.out.println(queue.peekFront() + " " + queue.peekEnd());
+        }
+        System.out.println(queue.isEmpty());
+        System.out.println(queue.isFull());
     }
 
     private int[] array;// 内置数组
-    private int front=0;// 头指针
-    private int end  =-1;// 尾指针
+    private int front = 0;// 头指针
+    private int end = -1;// 尾指针
+    private int count = 0; //队列中个数
 
-    public ArrayQueue(){
+    public ArrayQueue() {
         array = new int[10];
     }
 
-    public void insert(int i ){
-        array[++end] = i;
+    public void insert(int i) {
+        if(isFull()){
+            System.out.println("queue is full");
+            return;
+        }
+        if (end == array.length - 1) {
+            end = 0;
+        } else {
+            end++;
+        }
+        array[end] = i;
+        count++;
     }
 
-    public int remove(){
-        return array[front++] ;
+    public int remove() {
+        int tmp = array[front];
+        if (front == array.length - 1) {
+            front = 0;
+        } else {
+            front++;
+        }
+        count--;
+        return tmp;
     }
 
-    public boolean isEmpty(){
-        return (front==array.length || front ==0)?true:false;
+    public boolean isEmpty() {
+        return (count == 0) ? true : false;
     }
 
-    public boolean isFull(){
-        return (end == array.length-1)?true:false;
+    public boolean isFull() {
+        return (count == array.length) ? true : false;
     }
 
-    public int peekFront(){
+    public int peekFront() {
         return array[front];
     }
 
-    public int peekEnd(){
+    public int peekEnd() {
         return array[end];
     }
 }
