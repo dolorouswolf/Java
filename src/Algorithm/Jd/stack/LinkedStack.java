cf01dd75638dccdb28ce8fd1158cd274f304a246
zhangmin <zminds@163.com>
2017-03-02 22:08:22 +0800

    add:  链栈 通过链表实现的栈，top节点，为栈顶指针

---
+++ src/Algorithm/Jd/stack/LinkedStack.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright (c) 2017. zminds@163.com All Rights Reserved.
+ */
+
+package Algorithm.Jd.stack;
+
+/**
+ * rodney在02/03/2017创建。
+ */
+public class LinkedStack {
+    private Node top;
+
+    public LinkedStack(){
+        //链栈的栈底是一个null的节点，如果一个节点data和next都是null 则栈为空
+        this.top = new Node();
+    }
+
+    public void push(int i){
+        Node node = new Node(i);
+        node.next = top;
+        top = node;
+    }
+
+    public Node pop(){
+        Node node = top;
+        top = top.next;
+        return node;
+    }
+
+    public boolean isEmpty(){
+        return top.next == null;
+    }
+
+    public Node peek(){
+        return top;
+    }
+
+    public static void main(String[] args) {
+        LinkedStack linkedStack = new LinkedStack();
+        linkedStack.push(123);
+        linkedStack.push(124);
+        linkedStack.push(125);
+        System.out.println(linkedStack.peek().data);
+        System.out.println(linkedStack.isEmpty());
+        System.out.println(linkedStack.pop().data);
+        System.out.println(linkedStack.pop().data);
+        System.out.println(linkedStack.pop().data);
+        System.out.println(linkedStack.isEmpty());
+    }
+}
+
+class Node{
+    Node next;
+    int data;
+    public Node(int data){
+        this.data = data;
+    }
+
+    public Node(){
+        this.data = 0;
+        this.next = null;
+    }
+}
